eb85adc55822816a504e45c6e6e5d135247798bb
Matheus Guimarães <br.matheusguimaraes@gmail.com>
2023-10-31 04:44:44 -0300

    Update check-auth.js

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req,res,next)=>{
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.Authorization.split(" ")[1];
     jwt.verify(token,process.env.JWT_KEY)
     .then(()=>{
         next();
